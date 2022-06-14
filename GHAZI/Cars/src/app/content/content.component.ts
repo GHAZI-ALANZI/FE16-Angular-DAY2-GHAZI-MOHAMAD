8cecc08f19935fdcc535f22213e57de167cf4daa
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2022-06-14 12:38:10 +0200

    ok

--- GHAZI/Cars/src/app/content/content.component.ts
+++ GHAZI/Cars/src/app/content/content.component.ts
@@ -12,4 +12,16 @@ export class ContentComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  Cars: Array<{model:string,color:string,price:number,img:any}>=[
+    {model:"TZ",color:"red",price:200,img:"https://cdn.pixabay.com/photo/2016/09/15/12/12/renault-clio-1671405_960_720.jpg"},
+    {model:"TT",color:"white",price:300,img:"assets\img\yellow.jpg"},
+    {model:"TN",color:"black",price:500,img:"assets\img\yellow.jpg"},
+    {model:"TP",color:"yellow",price:700,img:"assets\img\yellow.jpg"},
+];
+
+
+  
+
+
+
 }
--- GHAZI/Cars/src/app/navbar/navbar.component.ts
+++ GHAZI/Cars/src/app/navbar/navbar.component.ts
@@ -12,4 +12,9 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  btnn:number=0;
+  increase(){
+    this.btnn+=10;
+  }
+
 }
